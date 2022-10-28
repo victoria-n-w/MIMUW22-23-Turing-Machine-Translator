9ba1eb0a3c03a2091fdaceed76961f30b40c868a
ajw <mj.wesolowski@student.uw.edu.pl>
2022-10-28 17:35:37 +0200

    uaaaaa

--- translator.cpp
+++ translator.cpp
@@ -16,6 +16,10 @@ struct LetterEncoding {
     std::string no_head, first_head, second_head, both_heads;
 };
 
+struct StateEncoding {
+    std::string start_scanning, going_back;
+};
+
 struct pairhash {
     size_t operator()(const std::pair<std::string, std::string> &p) const {
         return std::hash<std::string>()(p.first + p.second);
@@ -26,24 +30,88 @@ class Translation {
     typedef std::string State;
     typedef std::string Letter;
 
+    typedef std::unordered_map<std::pair<std::string, std::string>,
+                               LetterEncoding, pairhash>
+        LettersMap;
+
   public:
     Translation(const TuringMachine &intput);
 
     TuringMachine result();
 
   private:
-    void create_double_letters_();
+    LettersMap create_double_letters_();
 
-    std::unordered_map<std::pair<std::string, std::string>, LetterEncoding,
-                       pairhash>
-        letters_map_;
+    ImportantIdents create_important_idents_();
+
+    std::unordered_map<std::string, StateEncoding> create_state_aliases_();
+
+    void program_setup_protocol_(); // TODO
+
+    void program_scanning_for_letters_(); // TODO
+
+    void program_transitions_(); // TODO
+
+    void program_expanding_the_word_(); // TODO
+
     const TuringMachine &input_;
     SymbolSet states_, letters_;
+    LettersMap letters_map_;
+    ImportantIdents importandt_idents_;
+    std::unordered_map<std::string, StateEncoding> state_aliases_;
 };
 
 Translation::Translation(const TuringMachine &input)
-    : input_(input), states_(), letters_(input.working_alphabet()) {
-    create_double_letters_();
+    : input_(input),
+      // we cannot allow for those identifiers to be generated
+      states_(std::vector<std::string>{INITIAL_STATE, ACCEPTING_STATE,
+                                       REJECTING_STATE}),
+      letters_(input.working_alphabet()),
+      letters_map_(create_double_letters_()),
+      importandt_idents_(create_important_idents_()),
+      state_aliases_(create_state_aliases_()) {
+    program_setup_protocol_();
+
+    program_scanning_for_letters_();
+
+    program_transitions_();
+
+    program_expanding_the_word_();
+}
+
+Translation::LettersMap Translation::create_double_letters_() {
+    LettersMap res;
+    for (const Letter &letter_top : input_.working_alphabet()) {
+        for (const Letter &letter_bottom : input_.working_alphabet()) {
+            res[std::make_pair(letter_top, letter_bottom)] = LetterEncoding{
+                .no_head = letters_.generate(letter_top + "-" + letter_bottom),
+                .first_head =
+                    letters_.generate(letter_top + "_H-" + letter_bottom),
+                .second_head =
+                    letters_.generate(letter_top + "-" + letter_bottom + "_H"),
+                .both_heads = letters_.generate(letter_top + "_H-" +
+                                                letter_bottom + "_H")};
+        }
+    }
+    return res;
+}
+
+ImportantIdents Translation::create_important_idents_() {
+    return ImportantIdents{.letter_tape_start = letters_.generate("tape-start"),
+                           .letter_word_end = letters_.generate("word-end"),
+                           .semi_start = letters_.generate("semi-start")};
+}
+
+std::unordered_map<std::string, StateEncoding>
+Translation::create_state_aliases_() {
+    std::unordered_map<std::string, StateEncoding> res;
+    for (const State &state : input_.set_of_states()) {
+        res[state] = StateEncoding{
+            .going_back = states_.generate(state + "-going-back"),
+            .start_scanning = states_.generate(state + "-start-scanning"),
+        };
+    }
+    return res;
 }
 
 int main(int argc, char *argv[]) {
