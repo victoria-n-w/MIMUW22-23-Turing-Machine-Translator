a4cd6a0712676dd626400aac3451592c04a5d672
ajw <mj.wesolowski@student.uw.edu.pl>
2022-10-28 16:59:40 +0200

    ???"

--- translator.cpp
+++ translator.cpp
@@ -28,6 +28,8 @@ class Translation {
 
     // (old state) -> (new state, scanning for the first letter)
     std::unordered_map<std::string, std::string> state_aliases_;
+    std::unordered_map<State, State> state_cleanups_;
+    std::unordered_map<State, State> state_moving_second_word_;
 
     void create_new_symbols_() {
         important_idents_.semi_start = states_.generate("semi-start");
@@ -42,6 +44,18 @@ class Translation {
             second_head_over_letter_[letter] =
                 alphabet_.generate(letter + "__H");
         }
+
+        for (const auto &state : input_.set_of_states()) {
+            if (state == INITIAL_STATE) {
+                state_aliases_[state] = important_idents_.semi_start;
+                state_cleanups_[state] = states_.generate(state + "-cln");
+            } else if (state == ACCEPTING_STATE || state == REJECTING_STATE) {
+                state_aliases_[state] = state;
+            } else {
+                state_aliases_[state] = states_.generate(state);
+                state_cleanups_[state] = states_.generate(state + "-cln");
+            }
+        }
     }
 
     inline void new_transition_(const State &initial,
@@ -52,15 +66,34 @@ class Translation {
         const auto key = std::make_pair(initial, std::vector{old_letter});
 
         assert(res_transitions_.find(key) == res_transitions_.end());
-        res_transitions_[key] =
-            std::make_tuple(final, std::vector{new_letter}, head_move);
+        res_transitions_[key] = std::make_tuple(final, std::vector{new_letter},
+                                                std::string{head_move});
     }
 
     void program_setup_() {
         // TODO
     }
 
-    void program_head_move_(){};
+    void program_steps_cleanup_() {
+        for (const State &state : input_.set_of_states()) {
+            const State state_cleanup_alias = state_cleanups_[state];
+
+            for (const auto &letter : input_.working_alphabet()) {
+                new_transition_(state_cleanup_alias, letter,
+                                state_cleanup_alias, letter, HEAD_LEFT);
+            }
+
+            new_transition_(state_cleanup_alias,
+                            important_idents_.letter_tape_start,
+                            state_aliases_[state],
+                            important_idents_.letter_tape_start, HEAD_RIGHT);
+        }
+    }
+
+    void program_moving_second_word_(const State &target_state) {
+        const State moving_state = states_.generate(target_state + "-mv");
+        
+    };
 
     void program_transition_(const State &new_identifier,
                              const State &old_initial,
@@ -74,34 +107,54 @@ class Translation {
             return;
         }
 
-        // program letter change
-        const State move_second_head = states_.generate(
-            old_initial + "-" + first_letter + "-" + second_letter);
-
+        // program letter change and head move
+        const State second_head_erased = states_.generate(
+            old_initial + "-" + first_letter + "-" + second_letter + "-she");
         new_transition_(new_identifier, second_head_over_letter_[second_letter],
-                        move_second_head, second_letter, HEAD_STAY);
+                        second_head_erased, second_letter,
+                        std::get<2>(target->second)[1]);
 
-        // program_head_move
+        const State go_to_first_head = states_.generate(
+            old_initial + "-" + first_letter + "-" + second_letter + "-gtf");
+        for (const auto &letter : input_.working_alphabet()) {
+            // TODO blank???
+            new_transition_(second_head_erased, letter, go_to_first_head,
+                            second_head_over_letter_[letter], HEAD_LEFT);
+        }
 
-        // program_letter_change_();
+        new_transition_(go_to_first_head, important_idents_.letter_word_end,
+                        go_to_first_head, important_idents_.letter_word_end,
+                        HEAD_LEFT);
 
-        program_head_move_();
-
+        for (const auto &letter : input_.working_alphabet()) {
+            new_transition_(go_to_first_head, letter, go_to_first_head, letter,
+                            HEAD_LEFT);
+        }
         const State target_state = std::get<0>(target->second);
+        const State first_head_erased = states_.generate(
+            old_initial + "-" + first_letter + "-" + second_letter + "-fhe");
+
+        new_transition_(go_to_first_head, first_head_over_letter_[first_letter],
+                        first_head_erased, first_letter,
+                        std::get<2>(target->second)[0]);
+
+        for (const auto &letter : input_.working_alphabet()) {
+            new_transition_(first_head_erased, letter,
+                            state_cleanups_[target_state],
+                            first_head_over_letter_[letter], HEAD_LEFT);
+        }
+
+        if (state_moving_second_word_.find(target_state) ==
+            state_moving_second_word_.end()) {
+            program_moving_second_word_(target_state);
+        }
+
+        new_transition_(first_head_erased, important_idents_.letter_word_end,
+                        state_moving_second_word_[target_state],
+                        first_head_over_letter_[BLANK], HEAD_RIGHT);
     }
 
     void create_states_grid_() {
-
-        for (const auto &state : input_.set_of_states()) {
-            if (state == INITIAL_STATE) {
-                state_aliases_[state] = important_idents_.semi_start;
-            } else if (state == ACCEPTING_STATE || state == REJECTING_STATE) {
-                state_aliases_[state] = state;
-            } else {
-                state_aliases_[state] = states_.generate(state);
-            }
-        }
-
         for (const auto &state : input_.set_of_states()) {
 
             if (state == ACCEPTING_STATE || state == REJECTING_STATE) {
@@ -109,11 +162,6 @@ class Translation {
             }
 
             const auto looking_for_first_letter_ = state_aliases_[state];
-
-            new_transition_(looking_for_first_letter_,
-                            important_idents_.letter_tape_start,
-                            looking_for_first_letter_,
-                            important_idents_.letter_tape_start, HEAD_RIGHT);
 
             for (const auto &[first_letter, first_letter_with_head] :
                  first_head_over_letter_) {
@@ -132,13 +180,21 @@ class Translation {
                                 looking_for_second_letter,
                                 first_letter_with_head, HEAD_RIGHT);
 
+                // move over end of the word symbol
+                new_transition_(looking_for_second_letter,
+                                important_idents_.letter_word_end,
+                                looking_for_second_letter,
+                                important_idents_.letter_word_end, HEAD_RIGHT);
+
                 for (const auto &[second_letter, second_letter_with_head] :
                      second_head_over_letter_) {
 
+                    // didn't find the underlined letter
                     new_transition_(looking_for_second_letter, second_letter,
                                     looking_for_second_letter, second_letter,
                                     HEAD_RIGHT);
 
+                    // found the underlined letter
                     program_transition_(looking_for_second_letter, state,
                                         first_letter, second_letter);
                 }
@@ -159,6 +215,8 @@ class Translation {
         create_new_symbols_();
 
         program_setup_();
+
+        program_steps_cleanup_();
 
         create_states_grid_();
     }
